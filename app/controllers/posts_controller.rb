f1df06ab753c834d260ec4957dd7c34076ef6a04
Kevin Sun <kevinsun0@gmail.com>
2017-01-08 11:31:22 -0500

    index posts and error fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,10 @@ class PostsController < ApplicationController
   before_action :correct_user,   only: [:edit, :update, :destroy]
   before_action :find_post, only: [:show]
 
+  def index
+    @posts = Post.all.paginate(page: params[:page])
+  end
+
   def new
     @post  = current_user.posts.build
     render :partial=> 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,6 @@ Rails.application.routes.draw do
 
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
-  resources :posts,          only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :posts,          only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :relationships,       only: [:create, :destroy]
 end
